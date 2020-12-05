7d6aa643ca6918f25bff814c5f3bec266200f4aa
Patrick Mackinder <paddymackinder@gmail.com>
2020-12-05 11:48:45 +0000

    Day 5 part 1

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import day1 from './day1';
 import day2 from './day2';
 import day3 from './day3';
 import day4 from './day4';
+import day5 from './day5';
 
 const parts = [
     [
@@ -19,6 +20,9 @@ const parts = [
     [
         day4.part1,
         day4.part2
+    ],
+    [
+        day5.part1
     ]
 ]
 
