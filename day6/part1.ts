38d3bceb106b7e7659e41b6722bc3f45d1702758
Patrick Mackinder <paddymackinder@gmail.com>
2020-12-06 16:20:53 +0000

    Day 6 part 2

--- day6/part1.ts
+++ day6/part1.ts
@@ -1,35 +1,13 @@
 import {getInputSplitByBlankLines} from "../common/inputUtils";
+import {groupQuestionsAnswered} from "./index";
 
-function personQuestionsAnswered(personAnswers: string): Map<string, number> {
-    const answers: Map<string, number> = new Map<string, number>();
-    for(let answer of personAnswers) {
-        answers.has(answer) ? answers.set(answer, answers.get(answer) + 1) : answers.set(answer, 1);
-    }
-    return answers;
-}
-
-function groupQuestionsAnswered(groupAnswersString: string): Array<Map<string, number>> {
-    const questionsAnswered: Array<Map<string, number>> = [];
-    const groupAnswers = groupAnswersString.split("\n");
-    for(let personAnswers of groupAnswers) {
-        questionsAnswered.push(personQuestionsAnswered(personAnswers));
-    }
-    return questionsAnswered;
-}
-
-function combineGroupAnswers(groupAnswers: Array<Map<string, number>>): Map<string, number> {
-    const combined: Map<string, number> = new Map<string, number>();
-    let answers;
-    for(let personAnswers of groupAnswers) {
-        answers = personAnswers.keys();
-        for(let answer of answers) {
-            combined.has(answer) ? combined.set(answer, combined.get(answer) + 1) : combined.set(answer, 1);
-        }
-    }
+function combineGroupAnswers(groupAnswers: Array<Set<string>>): Set<string> {
+    const combined: Set<string> = new Set<string>();
+    groupAnswers.forEach(personAnswers => personAnswers.forEach(combined.add, combined));
     return combined;
 }
 
-function countUniqueQuestionsAnswered(groupAnswers: Array<Map<string, number>>): number {
+function countUniqueQuestionsAnswered(groupAnswers: Array<Set<string>>): number {
     return combineGroupAnswers(groupAnswers).size;
 }
 
