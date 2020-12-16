bfb137f220f78f1714224f97f47b2bc9fd2c191b
Patrick Mackinder <paddymackinder@gmail.com>
2020-12-16 01:05:58 +0000

    Day 15 part 2

--- day15/index.ts
+++ day15/index.ts
@@ -1,5 +1,32 @@
 import part1 from "./part1";
+import part2 from "./part2";
+import {getInputLines} from "../common/inputUtils";
+
+export function numbersGame(gameLength: number): number {
+    const spokenPositions: Map<number, number> = new Map<number, number>()
+    const startingNumbers: number[] = getInputLines(15)[0].split(',').map(number => parseInt(number));
+    startingNumbers.slice(0, startingNumbers.length-1).forEach((value, index) => {
+        spokenPositions.set(value, index+1);
+    })
+
+    let current: number = startingNumbers[startingNumbers.length-1];
+    let position: number = startingNumbers.length;
+    let difference;
+    while (position < gameLength) {
+        if(!spokenPositions.has(current)) {
+            spokenPositions.set(current, position++);
+            current = 0;
+        } else {
+            difference = position - spokenPositions.get(current);
+            spokenPositions.set(current, position++);
+            current = difference;
+        }
+    }
+
+    return current;
+}
 
 export default {
-    part1
+    part1,
+    part2
 }
